3bc8fcf859ac3323f775d4b8f3ffbab5e2b90d18
Ray <aaxx.2xx@gmail.com>
2019-02-08 16:55:09 +0800

    [fix] 处理图片压缩之后损坏的问题

--- webpack.base.js
+++ webpack.base.js
@@ -124,25 +124,6 @@ module.exports = {
           },
           {
             loader: 'image-webpack-loader',
-            options: {
-              mozjpeg: {
-                progressive: true,
-                quality: 65
-              },
-              optipng: {
-                enabled: false,
-              },
-              pngquant: {
-                quality: '65-90',
-                speed: 4
-              },
-              gifsicle: {
-                interlaced: false,
-              },
-              webp: {
-                quality: 75
-              }
-            }
           }
         ]
       },
